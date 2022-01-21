5f386e5d60b6012e98aeb1f39dc0df17c3c2d968
helloamandaball <helloamandaball@gmail.com>
2022-01-21 08:54:54 -0500

    minor adjustment to objects

--- enemies-list/js/enemiesList/main.js
+++ enemies-list/js/enemiesList/main.js
@@ -10,42 +10,42 @@ const GetEnemies = () => {
     // Make a list of Enemy objects
     let Enemies = [
         {
-            "FirstName": "Joshua",
-            "LastName": "Flowers",
-            "Offenses": [
+            FirstName: "Joshua",
+            LastName: "Flowers",
+            Offenses: [
                 "Being a jerk to me in elementary school",
                 "Not being nice to me in elementary school"
             ],
-            "IsReallyHated": true
+            IsReallyHated: true
         },
         {
-            "FirstName": "Darth",
-            "LastName": "Vader",
-            "Offenses": [
+            FirstName: "Darth",
+            LastName: "Vader",
+            Offenses: [
                 "Cut off Luke's hand",
                 "Murdered all those kids",
                 "Unkind management practices"
             ],
-            "IsReallyHated": false
+            IsReallyHated: false
         },
         {
-            "FirstName": "Betty",
-            "LastName": "Rudelady",
-            "Offenses": [
+            FirstName: "Betty",
+            LastName: "Rudelady",
+            Offenses: [
                 "Phone calls in the theater",
                 "Phone calls on the bus",
                 "Phone calls in line at the grocery store",
                 "Poor conversationalist"
             ],
-            "IsReallyHated": true
+            IsReallyHated: true
         },
         {
-            "FirstName": "Leon",
-            "LastName": "Peck",
-            "Offenses": [
+            FirstName: "Leon",
+            LastName: "Peck",
+            Offenses: [
                 "Keeps giving me a hotplate"
             ],
-            "IsReallyHated": false
+            IsReallyHated: false
         }
     ]
     return Enemies
